ab1d1ce199c9dd3c5c93af01fb5897fda7267e48
cmarano23 <cmarano23@amherst.edu>
2023-02-16 23:38:29 -0500

    initial commit

---
+++ assignment_03/title.js
@@ -0,0 +1,19 @@
+function drawGrid() {
+    const boxElement = document.querySelector(".grid-box");
+    for (let i = 0; i < 10; i++) {
+        var r = i * 10;
+        var g = i * 25;
+        var b = 90;
+        for (let j = 0; j < 10; j++) {
+            b = b + (j * 15)
+            let box = document.createElement("div");
+            // box.classList.add("grid-box");
+            box.style.position = "relative";
+            box.style.float = "left";
+            box.style.width = "50px";
+            box.style.height = "50px";
+            box.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+            boxElement.appendChild(box);
+        }
+    }
+}
